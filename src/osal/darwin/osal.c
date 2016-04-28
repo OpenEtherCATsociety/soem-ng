a02b12be17deafee45c84b4a9847e16881fa3cb1
Henric Lindén <henric.linden@rt-labs.com>
2016-04-28 23:10:01 +0200

    slaveinfo util working.

---
+++ src/osal/darwin/osal.c
@@ -0,0 +1,24 @@
+
+#include <osal.h>
+#include <options.h>
+#include <log.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+void os_log (int type, const char * fmt, ...)
+{
+   va_list list;
+
+   switch(LOG_LEVEL_GET (type))
+   {
+   case LOG_LEVEL_DEBUG: printf ("DEBUG - "); break;
+   case LOG_LEVEL_INFO: printf ("INFO - "); break;
+   case LOG_LEVEL_WARNING: printf ("WARNING - "); break;
+   case LOG_LEVEL_ERROR: printf ("ERROR - "); break;
+   default: break;
+   }
+
+   va_start (list, fmt);
+   vprintf (fmt, list);
+   va_end (list);
+}
